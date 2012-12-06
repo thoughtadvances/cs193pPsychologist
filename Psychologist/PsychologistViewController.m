a1a09f38baeacca048bc936f9615dd6bf40b4c9b
Alex Dunn <thoughtadvances@icloud.com>
2012-12-06 12:10:40 +0545

    Complete psychologist demo

--- Psychologist/PsychologistViewController.m
+++ Psychologist/PsychologistViewController.m
@@ -25,6 +25,12 @@
         // Don't check here because a crash means the developer needs to change
         //  something
         [segue.destinationViewController setHappiness:self.diagnosis];
+    } else if ([segue.identifier isEqualToString:@"Celebrity"]) {
+        [segue.destinationViewController setHappiness:100];
+    } else if ([segue.identifier isEqualToString:@"Serious"]) {
+        [segue.destinationViewController setHappiness:20];
+    } else if ([segue.identifier isEqualToString:@"TVKook"]) {
+        [segue.destinationViewController setHappiness:50];
     }
 }
 
