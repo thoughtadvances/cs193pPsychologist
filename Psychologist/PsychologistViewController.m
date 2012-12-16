32a18f91218e6df6b4657acd0c6ef30212f83c74
thoughtadvances <thoughtadvances@icloud.com>
2012-12-16 11:54:13 +0545

    Replace the UISplitViewController's detail when necessary for both Psychologist1 and Dr. Pill

--- Psychologist/PsychologistViewController.m
+++ Psychologist/PsychologistViewController.m
@@ -14,6 +14,18 @@
 @end
 
 @implementation PsychologistViewController
+
+// Identical to the same function in RotatableViewController.m
+- (id <SplitViewBarButtonItemPresenter>)splitViewBarButtonItemPresenter
+{
+    id detailVC = [self.splitViewController.viewControllers lastObject];
+    if (![detailVC conformsToProtocol:
+          @protocol(SplitViewBarButtonItemPresenter)]) {
+        detailVC = nil;
+    }
+    return detailVC;
+}
+
 - (HappinessViewController *)splitViewHappinessViewController {
     id happinessViewController = [self.splitViewController.viewControllers
                                   lastObject];
@@ -26,9 +38,23 @@
 
 - (void)setAndShowDiagnosis:(int)diagnosis {
     self.diagnosis = diagnosis;
-    if ([self splitViewHappinessViewController]) {
+    if ([self splitViewHappinessViewController]) { // If in UISplitView
+        // Just set happiness
         [[self splitViewHappinessViewController] setHappiness:diagnosis];
+        // Otherwise, segue using ShowDiagnosis
     } else [self performSegueWithIdentifier:@"ShowDiagnosis" sender:self];
+}
+
+- (void)transferSplitViewBarButtonItemToViewController:
+(id)destinationViewController { // Send a UIBarButtonItem to a new controller
+    NSLog(@"Transfer is called");
+    UIBarButtonItem *splitViewBarButtonItem =
+    [[self splitViewBarButtonItemPresenter] splitViewBarButtonItem];
+    [[self splitViewBarButtonItemPresenter] setSplitViewBarButtonItem:nil];
+    if (splitViewBarButtonItem) { // If we have a button, set it in destination
+        [destinationViewController setSplitViewBarButtonItem:
+         splitViewBarButtonItem];
+    }
 }
 
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
@@ -37,23 +63,56 @@
         //  something
         [segue.destinationViewController setHappiness:self.diagnosis];
     } else if ([segue.identifier isEqualToString:@"Celebrity"]) {
+        [self transferSplitViewBarButtonItemToViewController:
+         segue.destinationViewController];
         [segue.destinationViewController setHappiness:100];
     } else if ([segue.identifier isEqualToString:@"Serious"]) {
+        [self transferSplitViewBarButtonItemToViewController:
+         segue.destinationViewController];
         [segue.destinationViewController setHappiness:20];
     } else if ([segue.identifier isEqualToString:@"TVKook"]) {
+        [self transferSplitViewBarButtonItemToViewController:
+         segue.destinationViewController];
         [segue.destinationViewController setHappiness:50];
     }
 }
 
+- (void)handlePsychologistSegues {
+    // If coming from a UIViewController other than Psychologist1ViewController,
+    //  replace with Psychologist1ViewController
+    UIViewController *detailViewController =
+    [[self.splitViewController viewControllers] lastObject];
+    if (![detailViewController.title isEqualToString:
+          @"Psychologist1ViewController"]) { // It is the wrong UIViewController
+        UIViewController *Psychologist1ViewController = [self.storyboard instantiateViewControllerWithIdentifier: @"Psychologist1ViewController"];
+        NSMutableArray *viewControllers = [[self.splitViewController
+                                            viewControllers] mutableCopy];
+        [viewControllers replaceObjectAtIndex:([viewControllers count] - 1) withObject:Psychologist1ViewController];
+        self.splitViewController.viewControllers = viewControllers;
+        
+    }
+}
+
+// TODO: Get the toolbar button in these views, too.
+// TODO: Make these one function which sets happiness based on button title
 - (IBAction)flying {
+    if (self.splitViewController) { // If iPad, then handle segue differently
+        [self handlePsychologistSegues];
+    }
     [self setAndShowDiagnosis:85];
 }
 
 - (IBAction)apple {
+    if (self.splitViewController) { // If iPad, then handle segue differently
+        [self handlePsychologistSegues];
+    }
     [self setAndShowDiagnosis:100];
 }
 
 - (IBAction)dragons {
+    if (self.splitViewController) { // If iPad, then handle segue differently
+        [self handlePsychologistSegues];
+    }
     [self setAndShowDiagnosis:20];
 }
 
