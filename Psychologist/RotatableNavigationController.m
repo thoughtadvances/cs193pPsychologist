528584abc537c0f3f970a9649bb43c4056a22fdf
thoughtadvances <thoughtadvances@icloud.com>
2012-12-16 11:49:43 +0545

    Add custom class to make iPhone rotate to all orientations

---
+++ Psychologist/RotatableNavigationController.m
@@ -0,0 +1,21 @@
+//
+//  RotatableNavigationController.m
+//  Psychologist
+//
+//  Created by admin on 15/D/12.
+//  Copyright (c) 2012 ThoughtAdvances. All rights reserved.
+//
+
+#import "RotatableNavigationController.h"
+
+@interface RotatableNavigationController ()
+
+@end
+
+@implementation RotatableNavigationController
+
+- (NSUInteger)supportedInterfaceOrientations {
+    return UIInterfaceOrientationMaskAll;
+}
+
+@end
